7b41bdc6362bbb6e48090ac49a701c599b135189
darshu-123 <108139836+darshu-123@users.noreply.github.com>
2022-08-25 22:45:49 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays{
+	public static void main(String args[]){
+		int[]numbers={10,20,30,40,50,60,70};
+		for(int number:numbers){
+			System.out.println("Array:"+number);
+		}
+	}
+}
